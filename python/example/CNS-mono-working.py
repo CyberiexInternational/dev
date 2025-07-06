ad0677206d13ec3f8240cbd7407929542392b7d5
Toby McGowan <cyberiex.co.uk@gmail.com>
2025-07-06 13:17:47 +0100

    Longitude and latitude working on GPS grab

--- python/example/CNS-mono-working.py
+++ python/example/CNS-mono-working.py
@@ -9,13 +9,11 @@ import spidev as SPI
 import datetime
 import re, subprocess
 import random
-#import digidevice 
+
 sys.path.append("..")
 from lib import LCD_1inch5
 from PIL import Image,ImageDraw,ImageFont
 from gpiozero import CPUTemperature
-#from digidevice import location
-
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -26,69 +24,40 @@ device = 0
 logging.basicConfig(level=logging.DEBUG)
 
 #Working display area top left and bottom right co-ords on screen
-watlx=10
-watly=30
-wabrx=220
-wabry=250
+tlx=10
+tly=30
+brx=220
+bry=250
 cpu = CPUTemperature()
-
-def getTime(): 
-        currenttime = datetime.datetime.now()
-        logging.info(currenttime)
-        timenow=str(currenttime.hour) + ":" + str(currenttime.minute) + ":" + str(currenttime.second)
-        logging.info(timenow)
-        return timenow
-
 
 try:
     # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #disp = LCD_1inch5.LCD_1inch5(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
     disp = LCD_1inch5.LCD_1inch5()
     # Initialize library.
     disp.Init()
     # Clear display.
     disp.clear()
 
-   
-    logging.info("draw text")
-    
-    # Create blank image for drawing.
+     #Set up fonts
     Font1 = ImageFont.truetype("../Font/Font00.ttf",10)
     Font2 = ImageFont.truetype("../Font/Font00.ttf",10)
     Font3 = ImageFont.truetype("../Font/Font00.ttf",50)
-    
-    i=0
-    logging.info("Start loop")
-        
+
     #Loop forever, once per quarter second, updating display each time
     while True:
         image1 = Image.new("RGB", (disp.width,disp.height ), "BLACK")
         draw = ImageDraw.Draw(image1)
-        #draw.ellipse((watlx-10, watly-10, wabrx+10, wabry+10), fill = (0,255,0))
-
-        draw.rectangle([(watlx-10, watly-10), (wabrx+10, wabry+10)], fill = "#007700", outline="#005500")
-        draw.rectangle([(watlx, watly), (wabrx, wabry)], fill = "BLACK", outline="#005500")
-        
-        logging.info("loop : " + str(i))
+        draw.rectangle([(tlx-10, tly-10), (brx+10, bry+10)], fill = "#007700", outline="#005500")
+        draw.rectangle([(tlx, tly), (brx, bry)], fill = "BLACK", outline="#005500")
         currenttime = datetime.datetime.now()
         logging.info(currenttime)
         timenow=currenttime.strftime("%H") + ":" + currenttime.strftime("%M") + ":" + currenttime.strftime("%S") + "    " + currenttime.strftime("%d") + " " + currenttime.strftime("%b") + " " + currenttime.strftime("%Y")
-        tempinfo=str(cpu.temperature)
-        #sigstr=str(random.randrange(1,100)) + "%"
-        
-        draw.text((watlx+5, watly + 15), timenow , fill = "#99ff99",font=Font1)
-        draw.text((watlx+5, watly + 25), "CPU Temp : " + tempinfo + "'C", fill = "#99ff99",font=Font2)
-                
+        tempinfo=str(cpu.temperature)       
+        draw.text((tlx+5, tly + 15), timenow , fill = "#99ff99",font=Font1)
+        draw.text((tlx+5, tly + 25), "CPU Temp : " + tempinfo + "'C", fill = "#99ff99",font=Font2)                
         image1 = image1.rotate(90)
         disp.ShowImage(image1)
-
-        #Lets get lat long of kilmorie road if poss
-        
-        
-        time.sleep(0.25)
-        #disp.clear()
-     
+        time.sleep(0.25)  
     time.sleep(2)
     disp.module_exit()
     logging.info("quit:")
@@ -98,3 +67,7 @@ except KeyboardInterrupt:
     disp.module_exit()
     logging.info("quit:")
     exit()
+
+
+
+    
--- python/example/CNS-mono.py
+++ python/example/CNS-mono.py
@@ -9,13 +9,12 @@ import spidev as SPI
 import datetime
 import re, subprocess
 import random
-#import digidevice 
+import socket
+import ast
 sys.path.append("..")
 from lib import LCD_1inch5
 from PIL import Image,ImageDraw,ImageFont
 from gpiozero import CPUTemperature
-#from digidevice import location
-
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -26,69 +25,59 @@ device = 0
 logging.basicConfig(level=logging.DEBUG)
 
 #Working display area top left and bottom right co-ords on screen
-watlx=10
-watly=30
-wabrx=220
-wabry=250
+tlx=10
+tly=30
+brx=220
+bry=250
 cpu = CPUTemperature()
-
-def getTime(): 
-        currenttime = datetime.datetime.now()
-        logging.info(currenttime)
-        timenow=str(currenttime.hour) + ":" + str(currenttime.minute) + ":" + str(currenttime.second)
-        logging.info(timenow)
-        return timenow
-
 
 try:
     # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #disp = LCD_1inch5.LCD_1inch5(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
     disp = LCD_1inch5.LCD_1inch5()
     # Initialize library.
     disp.Init()
     # Clear display.
     disp.clear()
 
-   
-    logging.info("draw text")
-    
-    # Create blank image for drawing.
+     #Set up fonts
     Font1 = ImageFont.truetype("../Font/Font00.ttf",10)
     Font2 = ImageFont.truetype("../Font/Font00.ttf",10)
     Font3 = ImageFont.truetype("../Font/Font00.ttf",50)
-    
+
+    file = open("/home/file2")
+    gpsdata=file.read()
+    file.close()
+    logging.info(gpsdata)
+    gpsdict=ast.literal_eval(gpsdata[2:len(gpsdata)-1])
+    lat=gpsdict["latitude"]
+    long=gpsdict["longitude"] 
+    print("Start listening...")
     i=0
-    logging.info("Start loop")
-        
     #Loop forever, once per quarter second, updating display each time
     while True:
+        i+=1
+        if i==4:
+            file = open("/home/file2")
+            gpsdata=file.read()
+            file.close()
+            gpsddict=ast.literal_eval(gpsdata[2:len(gpsdata)-1])
+            lat=gpsdict["latitude"]
+            long=gpsdict["longitude"]
+            i=0
         image1 = Image.new("RGB", (disp.width,disp.height ), "BLACK")
         draw = ImageDraw.Draw(image1)
-        #draw.ellipse((watlx-10, watly-10, wabrx+10, wabry+10), fill = (0,255,0))
-
-        draw.rectangle([(watlx-10, watly-10), (wabrx+10, wabry+10)], fill = "#007700", outline="#005500")
-        draw.rectangle([(watlx, watly), (wabrx, wabry)], fill = "BLACK", outline="#005500")
-        
-        logging.info("loop : " + str(i))
+        draw.rectangle([(tlx-10, tly-10), (brx+10, bry+10)], fill = "#007700", outline="#005500")
+        draw.rectangle([(tlx, tly), (brx, bry)], fill = "BLACK", outline="#005500")
         currenttime = datetime.datetime.now()
         logging.info(currenttime)
         timenow=currenttime.strftime("%H") + ":" + currenttime.strftime("%M") + ":" + currenttime.strftime("%S") + "    " + currenttime.strftime("%d") + " " + currenttime.strftime("%b") + " " + currenttime.strftime("%Y")
-        tempinfo=str(cpu.temperature)
-        #sigstr=str(random.randrange(1,100)) + "%"
-        
-        draw.text((watlx+5, watly + 15), timenow , fill = "#99ff99",font=Font1)
-        draw.text((watlx+5, watly + 25), "CPU Temp : " + tempinfo + "'C", fill = "#99ff99",font=Font2)
-                
+        tempinfo=str(cpu.temperature)       
+        draw.text((tlx+5, tly + 15), timenow , fill = "#99ff99",font=Font1)
+        draw.text((tlx+5, tly + 25), "CPU Temp : " + tempinfo + "'C", fill = "#99ff99",font=Font2)                
+        draw.text((tlx+5, tly + 45), "Lat: " + str(lat) + "  Long: " + str(long), fill = "#99ff99",font=Font2)                 
         image1 = image1.rotate(90)
         disp.ShowImage(image1)
-
-        #Lets get lat long of kilmorie road if poss
-        
-        
-        time.sleep(0.25)
-        #disp.clear()
-     
+        time.sleep(0.25)  
     time.sleep(2)
     disp.module_exit()
     logging.info("quit:")
@@ -98,3 +87,7 @@ except KeyboardInterrupt:
     disp.module_exit()
     logging.info("quit:")
     exit()
+
+
+
+    
